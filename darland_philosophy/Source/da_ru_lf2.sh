97df5d073b7259a410d97886fc25ae66cae935b9
djdarland <student@dennisdarland.com>
2021-02-06 02:13:02 -0600

    Work toward making testing easier.

--- darland_philosophy/Source/da_ru_lf2.sh
+++ darland_philosophy/Source/da_ru_lf2.sh
@@ -30,6 +30,7 @@ echo "5 rules_0003.lf - Rules on Atomic Belief & Understanding"
 echo "6 rules_0004.lf - Rules on Opacity of Atomic Belief"
 echo "7 rules_0005.lf - Rules on Molecular Belief"
 echo "8 rules_0006.lf - Rules on True Atomic Belief"
+echo "9 rules_0007.lf - Rules for denounced Tests"
 
 echo "-----------"
 if test -f da_ru_lf2.sh
@@ -49,6 +50,7 @@ case $select in
 6) $EDITOR ../DarlandPhilosophyLife/RULES2/rules_0004.lf;;
 7) $EDITOR ../DarlandPhilosophyLife/RULES2/rules_0005.lf;;
 8) $EDITOR ../DarlandPhilosophyLife/RULES2/rules_0006.lf;;
+9) $EDITOR ../DarlandPhilosophyLife/RULES2/rules_0007.lf;;
 666) $EDITOR da_ru_lf2.sh;;
 777) $EDITOR NOTES_DJD.txt;;
 
