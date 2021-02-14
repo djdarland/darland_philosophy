bdd497a54e66d5f8ba532a5f2df94183738c652f
djdarland <student@dennisdarland.com>
2021-02-14 00:32:54 -0600

    Did clean up on comments and script files.

--- darland_philosophy/Source/cphtml.sh
+++ darland_philosophy/Source/cphtml.sh
@@ -1,16 +1,31 @@
-cd ../D*
+cd ../D*e
 cp FACTS2pre/facts_0000.lf HTML/FACTS2pre/facts_0000.lf.txt
+cp FACTS2pre/facts_0006.lf HTML/FACTS2pre/facts_0006.lf.txt
 cp IN2pre/darland_0021.lf HTML/IN2pre/darland_0021.lf.txt
 cp IN2pre/darland_0022.lf HTML/IN2pre/darland_0022.lf.txt
 cp IN2pre/darland_0023.lf HTML/IN2pre/darland_0023.lf.txt
 cp IN2pre/darland_0024.lf HTML/IN2pre/darland_0024.lf.txt
+cp IN2pre/darland_0025.lf HTML/IN2pre/darland_0025.lf.txt
+cp IN2pre/darland_0026.lf HTML/IN2pre/darland_0026.lf.txt
+cp IN2pre/darland_0027.lf HTML/IN2pre/darland_0027.lf.txt
 cp OUT2/darland_0021.out HTML/OUT2/darland_0021.out.txt
 cp OUT2/darland_0022.out HTML/OUT2/darland_0022.out.txt
 cp OUT2/darland_0023.out HTML/OUT2/darland_0023.out.txt
 cp OUT2/darland_0024.out HTML/OUT2/darland_0024.out.txt
+cp OUT2/darland_0025.out HTML/OUT2/darland_0025.out.txt
+cp OUT2/darland_0026.out HTML/OUT2/darland_0026.out.txt
+cp OUT2/darland_0027.out HTML/OUT2/darland_0027.out.txt
 cp RULES2/life_0001.lf HTML/RULES2/life_0001.lf.txt
-cp RULES2/life_0002.lf HTML/RULES2/life_0002.lf.txt
 cp RULES2/rules_0001.lf HTML/RULES2/rules_0001.lf.txt
 cp RULES2/rules_0002.lf HTML/RULES2/rules_0002.lf.txt
+cp RULES2/rules_0004.lf HTML/RULES2/rules_0004.lf.txt
 cp RULES2/rules_0006.lf HTML/RULES2/rules_0006.lf.txt
+cp RULES2/rules_0007.lf HTML/RULES2/rules_0007.lf.txt
+cp RULES2/rules_0008.lf HTML/RULES2/rules_0008.lf.txt
+cp RULES2/rules_0009.lf HTML/RULES2/rules_0009.lf.txt
+cp RULES2/rules_0011.lf HTML/RULES2/rules_0011.lf.txt
+
+
+
+
 
