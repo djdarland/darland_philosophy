a5d27981a30f97ee90f2f12e540d4bdac02fb882
djdarland <dennis.darland@hotmail.com>
2024-06-21 00:22:49 -0500

    Finished preliminary debugging of non-Whitehead logic and got syntax errors out of Whitehead part. All still needs to be gone over better.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   darland_0037.err
    	modified:   darland_0038.err
    	modified:   darland_0039.err
    	modified:   darland_0040.err
    	deleted:    ../ERRDIFF2/darland_0037.errdiff
    	deleted:    ../ERRDIFF2/darland_0038.errdiff
    	deleted:    ../ERRDIFF2/darland_0039.errdiff
    	deleted:    ../ERRDIFF2/darland_0040.errdiff
    	deleted:    ../ERRDIFF2/darland_0042.errdiff
    	deleted:    ../ERRDIFF2/darland_0061.errdiff
    	modified:   ../FACTS2/facts_0031.lf
    	modified:   ../FACTS2/facts_0071.lf
    	modified:   ../FACTS3/facts_0031.wlr
    	modified:   ../FACTS3/facts_0071.wlr
    	modified:   ../IN2/darland_0035.lf
    	modified:   ../IN2/darland_0036.lf
    	modified:   ../IN2/darland_0037.lf
    	modified:   ../IN2/darland_0038.lf
    	modified:   ../IN2/darland_0039.lf
    	modified:   ../IN2/darland_0040.lf
    	modified:   ../IN3/darland_0037.wlr
    	modified:   ../IN3/darland_0038.wlr
    	modified:   ../OUT2/darland_0035.out
    	modified:   ../OUT2/darland_0036.out
    	modified:   ../OUT2/darland_0037.out
    	modified:   ../OUT2/darland_0038.out
    	modified:   ../OUT2/darland_0039.out
    	modified:   ../OUT2/darland_0040.out
    	modified:   ../OUT2/darland_0073.out
    	deleted:    ../REFDIFF2/darland_0034.refdiff
    	deleted:    ../REFDIFF2/darland_0036.refdiff
    	deleted:    ../REFDIFF2/darland_0037.refdiff
    	deleted:    ../REFDIFF2/darland_0038.refdiff
    	deleted:    ../REFDIFF2/darland_0039.refdiff
    	deleted:    ../REFDIFF2/darland_0040.refdiff
    	deleted:    ../REFDIFF2/darland_0042.refdiff
    	deleted:    ../REFDIFF2/darland_0045.refdiff
    	deleted:    ../REFDIFF2/darland_0046.refdiff
    	deleted:    ../REFDIFF2/darland_0047.refdiff
    	deleted:    ../REFDIFF2/darland_0048.refdiff
    	modified:   ../REFDIFF2/darland_0073.refdiff
    	modified:   ../REFOUT2/darland_0034.refout
    	modified:   ../REFOUT2/darland_0035.refout
    	modified:   ../REFOUT2/darland_0036.refout
    	modified:   ../REFOUT2/darland_0037.refout
    	modified:   ../REFOUT2/darland_0038.refout
    	modified:   ../REFOUT2/darland_0039.refout
    	modified:   ../REFOUT2/darland_0040.refout
    	modified:   ../REFOUT2/darland_0045.refout
    	modified:   ../REFOUT2/darland_0046.refout
    	modified:   ../REFOUT2/darland_0047.refout
    	modified:   ../REFOUT2/darland_0048.refout
    	modified:   ../RULES2/rules_0033.lf
    	modified:   ../RULES2/rules_0035.lf
    	modified:   ../RULES2/rules_0036.lf
    	modified:   ../RULES2/rules_0037.lf
    	modified:   ../RULES2/rules_0038.lf
    	modified:   ../RULES2/rules_0039.lf
    	modified:   ../RULES2/rules_0040.lf
    	modified:   ../RULES2/rules_0045.lf
    	modified:   ../RULES2/rules_0046.lf
    	modified:   ../RULES2/rules_0047.lf
    	modified:   ../RULES2/rules_0048.lf
    	modified:   ../RULES2/rules_0071.lf
    	modified:   ../RULES2/rules_0072.lf
    	modified:   ../RULES2/rules_0073.lf
    	modified:   ../RULES2/rules_0074.lf
    	modified:   ../RULES3/rules_0033.wlr
    	modified:   ../RULES3/rules_0037.wlr
    	modified:   ../RULES3/rules_0038.wlr
    	modified:   ../RULES3/rules_0039.wlr
    	modified:   ../RULES3/rules_0040.wlr
    	modified:   ../RULES3/rules_0072.wlr
    	modified:   ../editdiffs2.sh
    	modified:   ../pre.sh
    
     Untracked files:
    	../../DarlandPhilosophyLife/REFERR2/in.referr
    	../../DarlandPhilosophyLife/REFOUT2/in.refout
    	../facts_0033.wlr
    	../to_wild.sno

---
+++ darland_philosophy/DarlandPhilosophyWilder/editdiffs2.sh
@@ -0,0 +1,23 @@
+echo ./darland_0031 -- Matches Perfectly
+echo ./darland_0032 -- Matches Perfectly
+echo ./darland_0033 -- Matches Perfectly
+echo ./darland_0034 -- Matches Perfectly
+echo ./darland_0035 -- Matches Perfectly
+echo ./darland_0036 -- Matches Perfectly
+echo ./darland_0037 -- Matches Perfectly
+echo ./darland_0038 -- Matches Perfectly
+echo ./darland_0039 -- Matches Perfectly
+echo ./darland_0040 -- Matches Perfectly
+echo ./darland_0045 -- Matches Perfectly
+echo ./darland_0046 -- Matches Perfectly
+echo ./darland_0047 -- Matches Perfectly
+echo ./darland_0048 -- Matches Perfectly
+./work2.sh darland_0071
+./work2.sh darland_0072
+./work2.sh darland_0073
+./work2.sh darland_0074
+echo 18 Total Tests completed
+echo 14 Total Tests match
+echo 4 Total Tests differ
+echo 4 Total Tests out differ
+echo 0 Total Tests err differ
